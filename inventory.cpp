238010cd6e8b381baa7ea3e7f0be3baa8799aaf7
Minh Pham <minh.pham@utah.edu>
2017-02-17 18:11:44 -0700

    stupid output

--- inventory.cpp
+++ inventory.cpp
@@ -1,6 +1,5 @@
 #include "inventory.h"
 #include <iostream>
-//#include "FoodItem.h"
 #include <map>
 #include <queue>
 #include <vector>
@@ -31,12 +30,10 @@ inventory::inventory(string fileName)
       else if(line.find("End") == 0)
 	{
 	  end();
+	  // Stop reading the instructions and stop modifying inventories
 	  break;
 	}
-      else{
-      }
-      
-      
+      else{}          
    }
   in.close();
 }
@@ -45,10 +42,16 @@ inventory:: ~inventory()
 {
 
 }
+
+/*
+ * For every food item, find its code, name, life time 
+ * and add those information to the corresponding maps
+ */
 void inventory::addFoodItem(std::string & line)
 {
   istringstream ss(line);
-  string code,name,life;
+  string code,name;
+  int  life;
   while(true)
     {
       string word;
@@ -56,17 +59,20 @@ void inventory::addFoodItem(std::string & line)
       
       if(ss.fail())
 	break;
-
+      // get the UPC code of food item
       else if (word.compare("Code:") == 0)
 	{
 	  ss >> word;
-	  code = word;	
+	  code = word;
+	    
 	}
+      // get the life time of food item
       else if(word.compare("life:") == 0)
 	{
 	  ss >> word;
-	  life = word;
+	  life = atoi(word.c_str());
 	}
+      // get the name of food item
       else if(word.compare("Name:") == 0)
 	{
 	  ss >> word;
@@ -78,17 +84,21 @@ void inventory::addFoodItem(std::string & line)
 	  }
 	}
     }
-
-  upc_days[code] = atoi(life.c_str());
+  // upc_days is a map of upc_code and its life time
+  upc_days[code] = life;
+  // upc_name is a map of upc_code and its name
   upc_name[code] = name;
   
  
 }
 
+/*
+ *Record the number of item the warehouse receives
+ */
 void inventory::addReceive(std::string & line)
 {
   istringstream ss(line);
-  string UPC, city;
+  string city,UPC;
   int quantity;
     
   while(true)
@@ -104,21 +114,28 @@ void inventory::addReceive(std::string & line)
 	  ss >> word;
 	  UPC=word;
 	  ss >> word;
+	  // quantities of the items that warehouse receives
 	  quantity=atoi(word.c_str());
 	  ss >> word;
+	  // name of the warehouse
 	  city=word;
 	}
     }
 
   for (int i=0; i<quantity; i++)
     {
+      // warehouse is a map.Each value is a map 
+      // with key is UPC_code of food item 
+      // and value contains all the received days of that food item
       warehouse[city][UPC].push(current);
     }
        
 }
 
-//void inventory::checkExpire( const queue <int> & dates, const int  & shelfLife)
-void inventory::checkExpire( queue <int> dates, const int shelfLife)
+/*
+ * Remove all the expired food
+ */
+void inventory::checkExpire( queue <int> & dates, const int shelfLife)
 {
   while(!dates.empty()&& ((this->current - dates.front()) > shelfLife)){
     dates.pop(); // if expired,pop from the queue
@@ -129,7 +146,7 @@ void inventory::addRequest(std::string & line)
 {
 
   istringstream ss(line);
-  string UPC, city;
+  string city, UPC;
   int quantity;
   
   while(true)
@@ -145,64 +162,73 @@ void inventory::addRequest(std::string & line)
 	  ss >> word;
 	  UPC=word;
 	  ss >> word;
+	  // requested quantities
 	  quantity=atoi(word.c_str());
 	  ss >> word;
 	  city=word;
 	}
     } 
-
-  int shelfLife = upc_days[UPC]; 
     
-  //update popular request map
-
+  // update popular requested map
+  // This map is used to show the popular products
   popular[UPC]+= quantity;
-  
-  if (warehouse.find(city) == warehouse.end()) // warehouse doesn't exist
-    { // do nothing
+
+  // warehouse doesn't exist  do nothin
+  if (warehouse.find(city) == warehouse.end())
+    { 
     }
   else
     {
-   
-      if (warehouse[city].find(UPC)==warehouse[city].end()) // item doesn't exist
-	{ // do nothing
+       // item doesn't exist do nothing
+      if (warehouse[city].find(UPC)==warehouse[city].end())
+	{ 
 	}
-      else // item exist
+      else // item exists
 	{	   
 	 
-	  if(warehouse[city][UPC].size() >= quantity)
+	  if((warehouse[city][UPC]).size() > quantity)
 	    {
 	      for (int i=0; i<quantity; i++)
 		{
-		  warehouse[city][UPC].pop();
+		  // remove items when it is requested
+		  (warehouse[city][UPC]).pop();
 		}
 	    }
 	  else
 	    {
-	      warehouse[city].erase(UPC); // if doesn't have enough, get rid of item
+	      // remove the food item from the warehouse if its stock is 
+	      // not enough to supply the request
+	      (warehouse[city]).erase(UPC); 
 	    }
 	}
     }
  
 }
 
-
+/*
+ * Increment the current day and remove all the expired food
+ */
 void inventory::nextDay()
 {
-    // create list of all stocked UPC
+   // Increment current day
+  this->current = this->current + 1;
+
   typedef map<string, queue<int> > InnerMap;
   typedef map<string, InnerMap> OuterMap;
-
-  map<string,int> stocked;
+  // Remove all expired food
   for(OuterMap::iterator outer_iter=warehouse.begin(); outer_iter!=warehouse.end(); ++outer_iter) {
     InnerMap &val = outer_iter->second;
     for(InnerMap::iterator inner_iter=val.begin(); inner_iter!=val.end(); ++inner_iter) {
+      
        int shelfLife = upc_days[inner_iter->first]; 
        checkExpire(inner_iter->second, shelfLife);
     }
   }
- 
-  this->current = this->current + 1;
 }
+
+/*
+ *Output the Unstocked Products, Well-stocked products, and Most requested products
+ */
 void inventory::end()
 {
   cout<<"Report by Minh Pham and To Tang"<<endl;
@@ -211,14 +237,28 @@ void inventory::end()
   // create list of all stocked UPC
   typedef map<string, queue<int> > InnerMap;
   typedef map<string, InnerMap> OuterMap;
-
+  // Stocked maps the food item with the number of warehouse that store it
   map<string,int> stocked;
+  // Search all the warehouses to add to stocked
   for(OuterMap::iterator outer_iter=warehouse.begin(); outer_iter!=warehouse.end(); ++outer_iter) {
     InnerMap &val = outer_iter->second;
+    cout<<endl;
+    cout<<outer_iter->first<<endl;
     for(InnerMap::iterator inner_iter=val.begin(); inner_iter!=val.end(); ++inner_iter) {
-      stocked[inner_iter->first]++;
+      cout<<inner_iter->first<<" "<<(inner_iter->second).size()<<endl;
+      if(!(inner_iter->second).empty())
+	stocked[inner_iter->first]++;
+      
     }
   }
+  // debug
+  for(map<string, int>::iterator it=stocked.begin(); it!=stocked.end();++it)
+    {      
+	cout<<it->first<<" "<<it->second<<endl;
+    }
+  
+  // Unstocked products are stored in none of the warehouse
+  // Their values in stocked map is 0
   cout<<"Unstocked Products:"<<endl;
   for(map<string, string>::iterator it=upc_name.begin(); it!=upc_name.end();++it)
     {
@@ -227,7 +267,8 @@ void inventory::end()
     }
   cout<<endl;
 
-
+  // Well-stocked products are stored in more than 1 warehouse
+  // Their values in stocked map is greater than 1
   cout<<"Well-Stocked Products:"<<endl;
   for(map<string, string>::iterator it=upc_name.begin(); it!=upc_name.end();++it)
     {
@@ -235,9 +276,13 @@ void inventory::end()
 	cout<<it->first<<" "<<it->second<<endl;
     }
   cout<<endl;
-  cout<<"Most Popular Products:"<<endl;
- // most popular
 
+  // Most popular products are requested the most
+  // Their values in the popular map are among the highest
+  cout<<"Most Popular Products:"<<endl;
+  // Find maximum value and remove its pair from the map
+  // Continue seaching the maximum value until we find the three most popular products
+  // (or less than 3 if the total food type is less than 3)
   int n = popular.size() < 3? popular.size():3;
   for(int i = 0; i < n; i++)
     {
@@ -256,25 +301,9 @@ void inventory::end()
     }
    cout<<endl;
 
-   /*
-  //debug map
-cout<<"stocked"<<endl;
-for(map<string, int>::iterator it=stocked.begin(); it!=stocked.end();++it)
-    {
-      
-	cout<<it->first<<" "<<it->second<<endl;
-    }
-cout<<"popular"<<endl;
-  for(map<string, int>::iterator it=popular.begin(); it!=popular.end();++it)
-    {
-      
-	cout<<it->first<<" "<<it->second<<endl;
-    }
-  
-   */
-
-
 }
+
+// MAIN
 int main(int argc, char *argv[] )
 {
   inventory h(argv[1]);
--- inventory.h
+++ inventory.h
@@ -10,22 +10,24 @@
 class inventory
 {
  private:
+  // warehouse is a map.Each value is a map with key is UPC_code of food item 
+  // and value contains all the received days of that food item
   std::map<std::string,std::map<std::string,std::queue<int> > > warehouse;
+  // upc_days is a map of upc_code and its life time
   std::map<std::string,int > upc_days;
+  // upc_name is a map of upc_code and its name
   std::map<std::string,std::string > upc_name;
+  // popular is a map of upc_code and its requested number
   std::map<std::string, int> popular;
   int current;
 
+  // helper method
   void addFoodItem(std::string & line);
   void addReceive(std::string & line);
-  void checkExpire(std::queue <int > dates, const int shelfLife );
+  void checkExpire(std::queue <int > & dates, const int shelfLife );
   void addRequest(std::string & line);
   void nextDay();
   void end();
-
- 
-  
-
 
  public:
   inventory(std::string _fileName);
